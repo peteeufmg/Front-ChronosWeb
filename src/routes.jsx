381597c67100e92ae11e1403ca89afaad98a04b2
Julio <teodoroalves.juliocesar@gmail.com>
2024-07-30 20:26:21 -0300

    init commit

---
+++ src/routes.jsx
@@ -0,0 +1,22 @@
+import {
+    createBrowserRouter,
+    createRoutesFromElements,
+    Route,
+    RouterProvider,
+} from "react-router-dom";
+
+import Home from "./Pages/Home";
+
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        //Colocar abaixo as rotas para as outras paginas do site
+        // Exemplo: para a página do seguidor colocar -> <Route path="seguidor" element={<[Pagina]/>}/>
+        <Route>
+            <Route path="/" element={<Home />} />
+        </Route>
+    )
+);
+
+export default function Routes() {
+    return <RouterProvider router={router} />
+}
