410d32d84bbefd6e831e83a514dd546430e5a5db
Julio <teodoroalves.juliocesar@gmail.com>
2024-08-01 15:36:03 -0300

    NavBar criada

---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,11 @@
+import { Container } from "./style";
+
+export default function NavBar() {
+    return(
+        <div>
+            <Container>
+                Oi
+            </Container>
+        </div>
+    )
+}
