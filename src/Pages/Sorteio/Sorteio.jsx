e08d429c8866d475db964a7cb22c135234ebf759
Julio <teodoroalves.juliocesar@gmail.com>
2024-08-22 18:57:02 -0300

    sorteio finalizado

--- src/Pages/Sorteio/Sorteio.jsx
+++ src/Pages/Sorteio/Sorteio.jsx
@@ -12,28 +12,31 @@ function shuffleArray(array) {
 
 function Sorteio() {
   const [dataSource, setDataSource] = useState([]);
-  const [selectedValue, setSelectedValue] = useState(null);
-  const [selectedStep, setSelectedStep] = useState(null);
-  const [step, setStep] = useState([]);
-  const [batteries, setBatteries] = useState([]);
+  const [dataToDraw, setDataToDraw] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [selectedRound, setSelectedRound] = useState(null);
+  const [selectedHeat, setSelectedHeat] = useState(null);
+  const [round, setRound] = useState([]);
+  const [heats, setHeats] = useState([]);
+  const [sentToBack, setSentToBack] = useState(false);
 
   useEffect(() => {
-    if (selectedValue !== null) {
-      if (selectedValue=='Avancada'){
+    if (selectedCategory !== null) {
+      if (selectedCategory=='Avancada'){
         const fetchData = async () => {
           try {
             const response = await api.get('/equipes', {params: {categoria:1}});
-            setDataSource(response.data); 
+            setDataToDraw(response.data); 
           } catch (error) {
             console.error("Erro ao buscar os dados:", error); 
           }
         };
         fetchData();
-      }else if (selectedValue=='Mirim'){
+      }else if (selectedCategory=='Mirim'){
         const fetchData = async () => {
           try {
             const response = await api.get('/equipes', {params: {categoria:2}});
-            setDataSource(response.data); 
+            setDataToDraw(response.data); 
           } catch (error) {
             console.error("Erro ao buscar os dados:", error); 
           }
@@ -41,35 +44,39 @@ function Sorteio() {
         fetchData();
       }
     }
-  }, [selectedValue]);
+  }, [selectedCategory]);
 
   useEffect(() => {
-    if (selectedValue=='Avancada'){
-      setStep([{ value:'Classificatória' },
+    if (selectedCategory=='Avancada'){
+      setRound([{ value:'Classificatória' },
         { value:'Repescagem' },
         { value:'Final' }])
-      if (selectedStep=='Classificatória'){
-        setBatteries([{value:'Bateria 1' },
+      if (selectedRound=='Classificatória'){
+        setHeats([{value:'Bateria 1' },
           { value:'Bateria 2' },
           { value:'Bateria 3'}
         ])
-      }if (selectedStep=='Repescagem'){
-        setBatteries([])
-      }if (selectedStep=='Final'){
-        setBatteries([])
+      }if (selectedRound=='Repescagem'){
+        setHeats([])
+        setSelectedHeat([]);
+      }if (selectedRound=='Final'){
+        setHeats([])
+        setSelectedHeat([]);
       }
-    }else if (selectedValue=='Mirim'){
-      setStep([{ value:'Classificatória' },
+    }else if (selectedCategory=='Mirim'){
+      setRound([{ value:'Classificatória' },
         { value:'Final' }])
-      if (selectedStep=='Classificatória'){
-        setBatteries([{value:'Bateria 1' },
+      if (selectedRound=='Classificatória'){
+        setHeats([{value:'Bateria 1' },
           { value:'Bateria 2' }
         ])
-      }else if (selectedStep=='Final'){
-        setBatteries([])
+      }else if (selectedRound=='Final'){
+        setHeats([]);
+        setSelectedHeat([]);
       }
     }
-  }, [selectedStep, selectedValue])
+    setSentToBack(false);
+  }, [selectedRound, selectedCategory, selectedHeat])
 
   const columns = [
     {
@@ -86,71 +93,119 @@ function Sorteio() {
       align: 'center',
     },
   ];
+
   const randomizeData = () => {
-    const shuffledData = shuffleArray([...dataSource]);
+    const shuffledData = shuffleArray([...dataToDraw]);
     setDataSource(shuffledData);
+    
+    if (!sentToBack) {
+      const equipesIDs = dataSource.map(e => ({id: e._id}));
+      const categoria = selectedCategory == "Avancada" ? 1 : 2;
+      const etapa = () => { 
+        switch (selectedRound) {
+          case "Classificatória": return 1;
+          case "Repescagem": return 2;
+          case "Final": return 3;
+        }
+      };
+      const bateria = () => { 
+        switch (selectedHeat) {
+          case "Bateria 1": return 1;
+          case "Bateria 2": return 2;
+          case "Bateria 3": return 3;
+        }
+      };
+      const body = {
+        categoria: categoria,
+        etapa: etapa(),
+        bateria: bateria(),
+        ordem: equipesIDs
+      };
+
+      //Arrumando essa função, tem q so permitir adicionar um sorteio.\
+      const postData = async () => {
+        try {
+          const request = await api.post("/sorteios", body);
+        } catch (error) {
+          let message = error;
+          switch (error.response.status) {
+            case 409: message = "O sorteio requisitado já foi realizado";
+            break;
+            case 500: message = error.response.data.message;
+            break;
+          }
+          alert(message);
+        }
+      }
+
+      postData();
+      setSentToBack(true);
+    }
   };
-    return (
-      <>
-        <Container>
-          <NavBar/>
-          <Frame>
-            <Title>
-              <SelectContainer>
-              <Text>Categoria:</Text> 
-              <Selection
-                placeholder="Selecionar"
-                filterSort={(optionA, optionB) =>
-                  (optionA?.label ?? '').toLowerCase().localeCompare((optionB?.label ?? '').toLowerCase())
-                }
-                onChange={value => setSelectedValue(value)}
-              >
-                <Option value="Avancada">Avançada</Option>
-                <Option value="Mirim">Mirim</Option>
-              </Selection>
-                
-              </SelectContainer>         
-              <SelectContainer>
-              <Text>Etapa:</Text> 
-              <Selection
-                placeholder="Selecionar"
-                filterSort={(optionA, optionB) =>
-                  (optionA?.label ?? '').toLowerCase().localeCompare((optionB?.label ?? '').toLowerCase())
-                }
-                onChange={value => setSelectedStep(value)}
-              >
-                {step.map(step => (
-                  <Option key={step.value} value={step.value}>
-                      {step.values}
-                </Option>
-                ))}
-              </Selection>
-              </SelectContainer>
-              <SelectContainer>
-              <Text>Bateria:</Text> 
-              <Selection
-                placeholder="Selecionar"
-                filterSort={(optionA, optionB) =>
-                  (optionA?.label ?? '').toLowerCase().localeCompare((optionB?.label ?? '').toLowerCase())
-                }
-              >
-                {batteries.map(battery => (
-                  <Option key={battery.value} value={battery.value}>
-                      {battery.values}
-                </Option>
-                ))}
-              </Selection>
-              </SelectContainer>
-                
-              <CustomButton onClick={randomizeData} text={"Sortear"}/>
-            </Title>
-            <ShuffleContainer>
-              <ShuffleTable dataSource={dataSource} columns={columns} pagination={false} />
-            </ShuffleContainer> 
-          </Frame>
-        </Container>
-      </>
-    )
+
+  return (
+    <>
+      <Container>
+        <NavBar/>
+        <Frame>
+          <Title>
+            <SelectContainer>
+            <Text>Categoria:</Text> 
+            <Selection
+              placeholder="Selecionar"
+              filterSort={(optionA, optionB) =>
+                (optionA?.label ?? '').toLowerCase().localeCompare((optionB?.label ?? '').toLowerCase())
+              }
+              onChange={value => setSelectedCategory(value)}
+            >
+              <Option value="Avancada">Avançada</Option>
+              <Option value="Mirim">Mirim</Option>
+            </Selection>
+              
+            </SelectContainer>         
+            <SelectContainer>
+            <Text>Etapa:</Text> 
+            <Selection
+              placeholder="Selecionar"
+              filterSort={(optionA, optionB) =>
+                (optionA?.label ?? '').toLowerCase().localeCompare((optionB?.label ?? '').toLowerCase())
+              }
+              onChange={value => setSelectedRound(value)}
+            >
+              {round.map(round => (
+                <Option key={round.value} value={round.value}>
+                    {round.values}
+              </Option>
+              ))}
+            </Selection>
+            </SelectContainer>
+            <SelectContainer>
+            <Text>Bateria:</Text> 
+            <Selection
+              placeholder="Selecionar"
+              filterSort={(optionA, optionB) =>
+                (optionA?.label ?? '').toLowerCase().localeCompare((optionB?.label ?? '').toLowerCase())
+              }
+              onChange={value => setSelectedHeat(value)}
+              value={selectedHeat}
+            >
+              {heats.map(heat => (
+                <Option key={heat.value} value={heat.value}>
+                    {heat.values}
+              </Option>
+              ))}
+            </Selection>
+            </SelectContainer>
+              
+            <CustomButton onClick={randomizeData} text={"Sortear"}/>
+          </Title>
+          <ShuffleContainer>
+            <ShuffleTable dataSource={dataSource} columns={columns} pagination={false} />
+          </ShuffleContainer> 
+        </Frame>
+      </Container>
+    </>
+  )
 }
 
 export default Sorteio;
