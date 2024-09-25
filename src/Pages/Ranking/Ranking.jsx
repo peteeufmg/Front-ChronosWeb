fd453d46f35c716bb093117573149df6c2a6cf84
Julio <teodoroalves.juliocesar@gmail.com>
2024-09-25 12:41:35 -0300

    rankeando as equipes da classificatoria

--- src/Pages/Ranking/Ranking.jsx
+++ src/Pages/Ranking/Ranking.jsx
@@ -1,44 +1,102 @@
-import { ConfigProvider, Table, Tabs } from "antd";
+import { ConfigProvider, Flex, Select, Table, Tabs, Typography } from "antd";
 import { Container, RankContainer, RanksContainer, RankTitle } from "./style";
 import NavBar from "../../Components/NavBar" 
+import api from "../../Services/api";
+import { useEffect, useState } from "react";
+
+const { Title } = Typography;
 
 export default function Ranking() {
+    const [categoria, setCategoria] = useState(1);
+    const [round, setRound] = useState(0);
+    const [heat, setHeat] = useState(0);
+    const [disableRepescagem, setDisableRepescagem] = useState(false);
+    const [disableHeats, setDisableHeats] = useState(false);
+    const [disableHeat3, setDisableHeat3] = useState(false);
+    const [classificatorias, setClassificatorias] = useState([]);
+    const [rankedTeams, setRankedTeams] = useState([]);
 
-    const dataSource = [
-        {
-          key: '1',
-          name: 'Mike',
-          age: 32,
-          address: '10 Downing Street',
-        },
-        {
-          key: '2',
-          name: 'John',
-          age: 42,
-          address: '10 Downing Street',
-        },
-      ];
-      
+    const fetchClassificatorias = async () => {
+        try {
+            const response = await api.get("/classificatorias");
+
+            setClassificatorias(response.data);
+            setRankedTeams(equipesComMelhorTempo.sort((a, b) => a.melhorTempo - b.melhorTempo));
+            console.log(classificatorias);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    // Mapeia para encontrar o melhor tempo de cada equipe
+    const equipesComMelhorTempo = classificatorias.map(classificatoria => {
+        const { id_equipe, bateria } = classificatoria;
+
+        // Encontrar o melhor tempo de todas as baterias
+        const melhorTempo = bateria.reduce((menorTempo, b) => {
+            // Verifica os tempos totais das baterias
+            const tempos = [b.tempo_total_1, b.tempo_total_2];
+            const menorTempoBateria = Math.min(...tempos);
+            return Math.min(menorTempo, menorTempoBateria);
+        }, Infinity);
+
+        console.log(melhorTempo);
+
+        // Retornar o _id, nome da equipe, e o menor tempo
+        return {
+            _id: id_equipe._id,
+            nome: id_equipe.nome,
+            melhorTempo
+        };
+    });
+    
+
+    useEffect(() => {
+        switch (categoria) {
+            case 1:
+                setDisableHeats(false);
+                setDisableHeat3(false);
+                setDisableRepescagem(false);
+                switch (round) {
+                    case 0:
+                        break;
+                    default:
+                        setDisableHeats(true);
+                }
+                break;
+            case 2: 
+                setDisableHeats(false);
+                setDisableHeat3(true);
+                setDisableRepescagem(true);
+                break;
+            case 3:
+                setDisableHeats(false);
+                setDisableRepescagem(true);
+                break;
+        }
+        fetchClassificatorias();
+    }, [categoria, round, heat]);
+
     const columns = [
         {
-          title: 'Name',
-          dataIndex: 'name',
-          key: 'name',
+            title: "Posição",
+            key: "1",
+            width: 10
         },
         {
-          title: 'Age',
-          dataIndex: 'age',
-          key: 'age',
-          sorter: (a, b) => a.age - b.age,
-          defaultSortOrder: 'ascending'
+            title: "Equipe",
+            dataIndex: "nome",
+            key: "2",
+            width: 350
         },
-        {
-          title: 'Address',
-          dataIndex: 'address',
-          key: 'address',
-        },
+        { 
+            title: "Tempo",
+            dataIndex: "tempo",
+            key: "3",
+            width: 200
+        }
     ];
-
+    
     const theme = {
         components: {
             Table: {
@@ -47,30 +105,45 @@ export default function Ranking() {
         },
     };
 
-
     return(
         <ConfigProvider theme={theme}>
             <NavBar />
             <Container>
-                <RanksContainer>
-                    <RankContainer>
-                        <RankTitle>Avançado</RankTitle>
+                <Flex align="center" gap="large" vertical>
+                    {/* Select categoria, etapa e bateria */}
+                    <Flex gap="large">
+                        <Select 
+                            defaultValue={1}
+                            onChange={e => setCategoria(e)}
+                        >
+                            <Select.Option value={1}>Seguidor Avançado</Select.Option>
+                            <Select.Option value={2}>Seguidor Mirim</Select.Option>
+                            <Select.Option value={3}>Sumô</Select.Option>
+                        </Select>
+                        <Select 
+                            defaultValue={0}
+                            onChange={e => setRound(e)}
+                        >
+                            <Select.Option value={0}>Classificatória</Select.Option>
+                            <Select.Option value={1} disabled={disableRepescagem}>Repescagem</Select.Option>
+                            <Select.Option value={2}>Final</Select.Option>
+                        </Select>
+                        <Select
+                            defaultValue={0}
+                            disabled={disableHeats}
+                            onChange={e => setHeat(e)}
+                        >
+                            <Select.Option value={0}>Bateria 1</Select.Option>
+                            <Select.Option value={1}>Bateria 2</Select.Option>
+                            <Select.Option disabled={disableHeat3} value={2}>Bateria 3</Select.Option>
+                        </Select>
+                    </Flex>
+                    <Flex>
                         <Table
-                            dataSource={dataSource}
                             columns={columns}
-                            sortDirections={["ascend"]}
-                            pagination={{position: ["none", "none"]}}
                         />
-                    </RankContainer>
-                    <RankContainer>
-                        <RankTitle>Mirim</RankTitle>
-                        <Table 
-                            dataSource={dataSource}
-                            columns={columns}
-                            pagination={{position: ["none", "none"]}} 
-                        />
-                    </RankContainer>
-                </RanksContainer>
+                    </Flex>
+                </Flex>
             </Container>
         </ConfigProvider>
     )
